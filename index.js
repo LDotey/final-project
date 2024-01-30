594abd758382b9d293a88083b7d63198f4dcacdf
Leigh Dotey <leighdotey@gmail.com>
2024-01-29 20:49:50 -0500

    create team cards

--- index.js
+++ index.js
@@ -5,6 +5,40 @@ function init () {
     .then((resp) => resp.json())
     .then((teamsArray) => {
         const teamsList = document.getElementById("team-collection")
+        teamsArray.forEach(team => {
+            const teamCard = document.createElement('div')
+            teamCard.classList = "card"
+            teamsList.append(teamCard)
+
+            const teamName = document.createElement('h2')
+            teamName.innerHTML = team.full_name
+            teamCard.append(teamName)
+
+            const teamLogo = document.createElement('img')
+            teamLogo.src = team.logo
+            teamCard.append(teamLogo)
+
+            const trophyCount = document.createElement('p')
+            trophyCount.innerHTML = 
+            teamCard.append(trophyCount)
+
+            const rosterWest = document.getElementById("western-conference")
+            const rosterEast = document.getElementById("eastern-conference")
+            
+            const teamConf = document.createElement('option')
+            teamConf.value = team.conference
+            
+            if (teamConf.textContent === "West") {
+                rosterWest.append(team.city)
+            } else if (teamConf.textContent === "East") {
+                rosterEast.append(team.city)
+            }
+
+
+            console.log(teamConf.textContent)
+
+    })
+
 
 })
 }
